df33f9ac38eb986c6bc777c1be70f0d93b6cab1c
Matt Bailey <bailey.matthewr@gmail.com>
2021-05-04 17:57:50 -0600

    Add more examples with notes

--- 2-modern-js/8-destructuring.js
+++ 2-modern-js/8-destructuring.js
@@ -15,18 +15,34 @@ const { PI, E, SQRT2 }  = Math;
  * it uses the 'require' syntax. It works the same way though.
  */
 
-// const circle = {
-//   label: 'circleX',
-//   radius: 2,
-// };
-//
-// const circleArea = ({ radius }) =>
-//   (PI * radius * radius).toFixed(2);
-//
-// console.log(
-//   circleArea(circle)
-// );
+const circle = {
+  label: 'circleX',
+  radius: 2,
+};
+
+const circleArea = ({ radius }) =>
+    (PI * radius * radius).toFixed(2);
+
+console.log(
+    circleArea(circle)
+);
 /* Lines 18 through 28 show an example of destructuring an object. This can,
  * in some ways, lead to readability of a function. 'circle' is destructured
  * to get it's radius property and only work with that.
+ */
+
+const circleArea2 = ({ radius }, { precision=2 } = {}) =>
+    (PI * radius * radius).toFixed(precision);
+
+console.log(
+    circleArea2(circle, { precision: 5 })
+);
+/* Lines 34 through 39 show an example of adding default values for
+ * destructuring. This can also be pretty helpful when you don't know what all
+ * may call the function.
+ */
+
+const [first, second,, fourth] = [10, 20, 30, 40];
+/* This line above can pull out the first, second, and fourth values or objects
+ * from another object, including an array.
  */

8b6b930179bbbcc18ee6fc44842759007859f9d7
Matt Bailey <bailey.matthewr@gmail.com>
2021-05-05 16:53:32 -0600

    Add notes to templating API

--- 4-modules/2-examples/4-use.js
+++ 4-modules/2-examples/4-use.js
@@ -1,5 +1,10 @@
 const templateGenerator = require('./4-function');
+//templateGenerator is a function. This api is returning the function stored
 
 const myTemplate = templateGenerator('Hello Node!');
+//myTemplate is the template returned from calling the function.
 
 console.log(myTemplate);
+//while this does print out the completed template, this wouldn't be how you do
+//this in a real application. HTML could be generated within a page, calling
+//this API.
